a21af1d736a5f0e182f9a6a7f1878b69c461c6a4
Dionkeldei <dionkeldei@gmail.com>
2021-02-03 13:35:21 -0600

    getting variables

--- src/Parser.php
+++ src/Parser.php
@@ -4,11 +4,25 @@ namespace dionkeldei\BladeParser;
 
 class Parser
 {
-    function __construct(html){
-      $this->html = html;
+    function __construct($html){
+      $this->html = $html;
+      $this->variables = [];
     }
+
 
     function showHtml(){
       return $this->html;
     }
+
+    function getVariables(){
+      preg_match_all('/\\{{(.*?)\\}}/', $this->html, $match);
+      foreach($match[1] as $var){
+        if(strpos($var,"$") == 0){
+          $var = explode('$',$var);
+          array_push($this->variables,$var[1]);
+        }
+      }
+      return (object) $this->variables;
+    }
 }
+
